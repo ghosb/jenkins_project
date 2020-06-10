2a491327fc9e0d7a4c795ceb5ccc743ac87e8a58
ghosb <386662310@qq.com>
2020-06-10 17:31:58 +0800

    updata

---
+++ scripts/test_login.py
@@ -0,0 +1,12 @@
+class TestLogin:
+    def test_login_001(self):
+        assert 0
+
+
+    def test_login_002(self):
+        assert 1
+
+    def test_login_003(self):
+        assert 1
+
+        '''https://github.com/ghosb/jenkins_project.git'''
